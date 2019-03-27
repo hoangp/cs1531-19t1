08e64d9c16bdc53fb9bf8de39c86cd893d9046e0
Hoang Pham <hoang.unsw@gmail.com>
2019-03-27 11:50:16 +1100

    tut06 final

--- tut06/TDD/interest.py
+++ tut06/TDD/interest.py
@@ -1,6 +1,6 @@
 # Step 1: Write an empty method
 def calculate_simple_interest(principal,interest,years):
-    pass
+    return principal*interest*years
 
 # Step 2: Write a test (that fails) for the above method and run the test
 # Step 3: Write just enough code to make the above test succeed
--- tut06/TDD/test_interest.py
+++ tut06/TDD/test_interest.py
@@ -2,9 +2,11 @@ import pytest
 from interest import calculate_simple_interest
 
 # Step 2: Write a test (that fails) for the above method and run the test
-#def  test_calculate_simple_interest():
-#	assert calculate_simple_interest(100,0.04,1) ==  4
+def  test_calculate_simple_interest():
+	assert calculate_simple_interest(100,0.04,1) == 4
 
+def  test_calculate_simple_interest_5years():
+	assert calculate_simple_interest(100,0.04,5) == 20
 
 # Step 3: Write just enough code to make the above test succeed
 # Step 4: Re-run the test and ensure that the test succeeds
